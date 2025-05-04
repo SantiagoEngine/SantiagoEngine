8744e8e695d45bd16ca8bafbdbbc86458565300d
Santiago <calebesantiago206@gmail.com>
2025-05-04 20:25:01 -0300

    Pretty sure i forgot ts

--- setup/Setup/setup-unix.sh
+++ setup/Setup/setup-unix.sh
@@ -16,3 +16,4 @@ haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
 haxelib install hxWindowColorMode
+haxelib install flxgif
