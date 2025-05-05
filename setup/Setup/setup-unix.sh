c08e5f05f44794734c7eb5040f16d9c608e3b1aa
Santiago <calebesantiago206@gmail.com>
2025-05-05 15:45:28 -0300

    test

--- setup/Setup/setup-unix.sh
+++ setup/Setup/setup-unix.sh
@@ -12,8 +12,9 @@ haxelib install flixel-tools
 haxelib install SScript
 haxelib install hxCodec
 haxelib install tjson
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
 haxelib install hxWindowColorMode
 haxelib install flxgif
+
